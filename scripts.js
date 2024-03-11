5ba3b608119f8b8860f87164f1c064a10d5892da
Alex <schwimmeralexander@gmail.com>
2024-03-11 01:16:30 -0700

    All button elements now have transition styles, JS will add a class that modifies appearance of operator buttons on click, add class to operator buttons in index so the class list can be removed from inactive operator buttons

--- scripts.js
+++ scripts.js
@@ -24,38 +24,53 @@ const calcNumbers = Array.from(document.querySelectorAll(".calc-button")).filter
 const display = document.getElementById("display");
 
 const clearButton = document.getElementById("clear").addEventListener("click", () => {
-    if(memory.operand1 !== "0" && memory.operand2 === "") {
-        memory.operand1 = "0";
-        display.innerHTML = memory.operand1;
-    }
-    else if(memory.operand2 !== "") {
-        memory.operand2 = "";
-        display.innerHTML = "0";
-    }
+    memory.operand1 = "0";
+    memory.operand2 = "";
+    display.innerHTML = "0";
+    memory.operator = null;
 })
-const sumButton = document.getElementById("sum").addEventListener("click", () => {
+const operatorButtons = Array.from(document.querySelectorAll(".operator-button"));
+const sumButton = document.getElementById("sum");
+sumButton.addEventListener("click", () => {
     memory.operator = calcSum;
+    operatorButtons.forEach((element) => {
+        element.classList.remove("operator-clicked-on");
+    })
+    sumButton.classList.add("operator-clicked-on");
 })
-const multiplyButton = document.getElementById("multiply").addEventListener("click", () => {
+
+const multiplyButton = document.getElementById("multiply");
+multiplyButton.addEventListener("click", () => {
     memory.operator = calcMultiply;
+    operatorButtons.forEach((element) => {
+        element.classList.remove("operator-clicked-on");
+    })
+    multiplyButton.classList.add("operator-clicked-on");
 })
-const subtractButton = document.getElementById("subtract").addEventListener("click", () => {
+const subtractButton = document.getElementById("subtract");
+subtractButton.addEventListener("click", () => {
     memory.operator = calcSubtract;
+    operatorButtons.forEach((element) => {
+        element.classList.remove("operator-clicked-on");
+    })
+    subtractButton.classList.add("operator-clicked-on");
 })
-const divideButton = document.getElementById("divide").addEventListener("click", () => {
+const divideButton = document.getElementById("divide");
+divideButton.addEventListener("click", () => {
     memory.operator = calcDivide;
+    operatorButtons.forEach((element) => {
+        element.classList.remove("operator-clicked-on");
+    })
+    divideButton.classList.add("operator-clicked-on");
 })
-const equalsButton = document.getElementById("equals").addEventListener("click", () => {
+const equalsButton = document.getElementById("equals");
+equalsButton.addEventListener("click", () => {
     if(memory.operand2 === "") {
         return;
     }
     let result = memory.operator(parseFloat(memory.operand1), parseFloat(memory.operand2));
     if (result > 100000000) {
         result = result.toExponential(2);
-        // const parts = result.split("e");
-        // const coefficient = parseFloat(parts[0]).toFixed(2);
-        // const exponent = parts[1];
-        // result = `${coefficient}e${exponent}`;
     } else if (result.toString().includes(".") && result.toString().split(".")[1].length > 8) {
         result = parseFloat(result.toFixed(8));
     }
@@ -63,6 +78,9 @@ const equalsButton = document.getElementById("equals").addEventListener("click",
     display.innerHTML = result;
     memory.operand1 = result.toString();
     memory.operand2 = "";
+    operatorButtons.forEach((element) => {
+        element.classList.remove("operator-clicked-on");
+    })
 });
 const pointButton = document.getElementById("point").addEventListener("click", () => {
     const point = ".";
@@ -103,7 +121,7 @@ const percentButton = document.getElementById("percent").addEventListener("click
 })
 const toggleNumberSignButton = document.getElementById("plus-minus").addEventListener("click", () => {
     let result;
-    if(!memory.operator) {
+    if(!memory.operator || memory.operand2 === "") {
         if(memory.operand1.length >= MAX_DISPLAY_SIZE) {
             return
         }
@@ -111,7 +129,8 @@ const toggleNumberSignButton = document.getElementById("plus-minus").addEventLis
         result *= -1;
         memory.operand1 = result.toString();
         display.innerHTML = memory.operand1;
-    } else {
+    } 
+    else {
         if(memory.operand2.length >= MAX_DISPLAY_SIZE) {
             return
         }
